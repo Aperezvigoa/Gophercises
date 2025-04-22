edb31e3213e57c832d466211117c7c280c6010f0
Aperezvigoa <108186387+Aperezvigoa@users.noreply.github.com>
2025-04-22 14:01:25 +0200

    First version of the program

---
+++ Go_Quiz_Game/main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+
+	// --- Opening the file
+	fi, err := os.Open("problems.csv")
+	if err != nil {
+		panic(err)
+	}
+
+	// --- Defer func to close the file when the program ends
+	defer func() {
+		if err := fi.Close(); err != nil {
+			panic(err)
+		}
+		fmt.Println("File closed.")
+	}()
+
+	// --- Getting a string slice divided by line from the CSV
+	grossFile := readCSV(fi)
+
+	// --- Getting the map Test, k is question, v is answer
+	mapTest := splitKeyValue(grossFile)
+
+	// --- Requesting user answers
+	fmt.Println("Try to think fast!")
+	answers := printingTest(mapTest)
+
+	// --- Getting the result
+	comparingResults(mapTest, answers)
+
+}
+
+// --- Creating a scanner to read the CSV line by line and writing it to a string slice
+
+func readCSV(file *os.File) []string {
+	scanner := bufio.NewScanner(file)
+	var grossFile []string
+
+	for scanner.Scan() {
+		grossFile = append(grossFile, scanner.Text())
+	}
+	if err := scanner.Err(); err != nil {
+		panic(err)
+	}
+
+	return grossFile
+}
+
+// --- Spliting key - value from the grossFile slice. Ex Key - [5+5] Value - [10], divide by ','
+
+func splitKeyValue(grossFile []string) map[string]string {
+	formatedFileMap := make(map[string]string)
+
+	for i := range grossFile {
+		grossLine := strings.Split(grossFile[i], ",")
+		formatedFileMap[grossLine[0]] = grossLine[1]
+	}
+	return formatedFileMap
+}
+
+// --- Printing Keys to console & receiving user answers
+
+func printingTest(fileLines map[string]string) map[string]string {
+	reader := bufio.NewReader(os.Stdin)
+	userAnswers := make(map[string]string)
+	for k := range fileLines {
+		fmt.Printf("%s:", k)
+		answer, err := reader.ReadString('\n')
+		answer = strings.TrimSpace(answer)
+		if err != nil {
+			fmt.Println("Error", err)
+		}
+
+		userAnswers[k] = answer
+	}
+	return userAnswers
+}
+
+// --- Comparing Results
+
+func comparingResults(fileLines map[string]string, answers map[string]string) {
+
+	var correctAnswers int
+	for k := range fileLines {
+		if fileLines[k] == answers[k] {
+			correctAnswers++
+		}
+	}
+	fmt.Printf("Total correct answers: %d/%d\n", correctAnswers, len(fileLines))
+}
